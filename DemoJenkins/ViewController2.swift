70713421b12422e2cb3e5fdc2cdbf26766d3c779
Devesh Tyagi <tyagi77@outlook.com>
2021-03-17 02:21:51 +0530

    ..

---
+++ DemoJenkins/ViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  DemoJenkins
+//
+//  Created by Devesh Tyagi on 17/03/21.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
